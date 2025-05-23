d2826b74c2a55a334421965104909e9745fdd9d5
Diswanthkumar26 <diswanthkumar.ch@gmail.com>
2025-05-23 14:55:07 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: process.env.VITE_BASE_PATH || "/my-dashboard",
